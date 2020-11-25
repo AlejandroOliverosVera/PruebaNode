115eb924722a86b756674d1c447f170c457b903c
AlejandroOliverosVera <alejandro.oliveros@alumnos.uv.cl>
2020-11-25 14:07:34 -0300

    Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import 'semantic-ui-css/semantic.min.css'
 import { Button, Icon, Label, Menu, Image} from 'semantic-ui-react'
 import MyModal from './MyModal';
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ReactPlayer from 'react-player'
 
 
 class Cambio extends React.Component{
@@ -32,7 +39,40 @@ class Cambio extends React.Component{
   }
 }
 
+class Video extends React.Component{
+  constructor(props){
+    super(props)
+    window.helloVideo = this;
+    this.state={
+      url:"https://www.youtube.com/watch?v=NvOHijJqups"
+    }
+  }
+  
+  componentDidMount() {
+    document.body.style.backgroundColor = "#133f5a"
+  }
 
+  MyVid = (a) =>{
+    this.setState({
+      url: a
+    })
+  }
+
+  render(){
+    return(
+      <div className="Videos">
+        <ReactPlayer
+          relative
+          url={this.state.url}
+          controls
+          playbackRate = {1}
+          width = "896px"
+          height = "504px"
+        />
+      </div>
+    )
+  }
+}
 
 function App() {
 
@@ -51,61 +91,121 @@ function App() {
   function gifMarine(){
     window.helloComponent.MyGif("https://media1.tenor.com/images/ae37217f57af05eff7586a69716a42f4/tenor.gif");
   }
-  
+
+  function vidIna(){
+    window.helloVideo.MyVid("https://www.youtube.com/watch?v=n-hRYCpm8wQ");
+  }
+
+  function vidFubuki(){
+    window.helloVideo.MyVid("https://www.youtube.com/watch?v=2o0eJ2ULwmo");
+  }
+
+  function vidMarine(){
+    window.helloVideo.MyVid("https://www.youtube.com/watch?v=e7VK3pne8N4");
+  }
+
   return (
-    <div className="App">
-      <Menu inverted>
-        <Menu.Item
-          name='Ina'          
-          onClick={gifIna}
-        >
-          Ina
-        </Menu.Item>
+    <Router>
+      <Switch>
+        <Route path = "/" exact>
+          <div className="App">
+            <Menu inverted>
+              <Menu.Item
+                name='Ina'          
+                onClick={gifIna}
+              >
+                Ina
+              </Menu.Item>
 
-        <Menu.Item
-          name='Fubuki'
-          
-          onClick={gifFubuki}
-        >
-          Fubuki
-        </Menu.Item>
+              <Menu.Item
+                name='Fubuki'
+                
+                onClick={gifFubuki}
+              >
+                Fubuki
+              </Menu.Item>
 
-        <Menu.Item
-          name='Marine'          
-          onClick={gifMarine}
-        >
-          Marine
-        </Menu.Item>
-      </Menu>
-      <Button as='div' labelPosition='right' onClick={(e,d)=>setOpenModal(true)}>
-      <Button icon>
-        <Icon name='heart' />
-        Like
-      </Button>
-      <Label as='a' basic pointing='left'>
-        2,048
-      </Label>
-    </Button>
-    <Button as='div' labelPosition='left'>
-      <Label as='a' basic pointing='right'>
-        2,048
-      </Label>
-      <Button icon>
-        <Icon name='heart' />
-        Like
-      </Button>
-    </Button>
-    <Button as='div' labelPosition='left'>
-      <Label as='a' basic>
-        2,048
-      </Label>
-      <Button icon>
-        <Icon name='fork' />
-      </Button>
-    </Button>
-    <MyModal visible={openModal} onClose={()=>{console.log("close"); setOpenModal(false);}} />
-    <Cambio/>
-    </div>
+              <Menu.Item
+                name='Marine'          
+                onClick={gifMarine}
+              >
+                Marine
+              </Menu.Item>
+            </Menu>
+            <Button >
+              <Link to="/">
+                Inicio
+              </Link>
+            </Button>
+            <Button >
+              <Link to="/videos">
+                Videos
+              </Link>
+            </Button>
+            <Button as='div' labelPosition='right' onClick={(e,d)=>setOpenModal(true)}>
+            <Button icon>
+              <Icon name='heart' />
+              Like
+            </Button>
+            <Label as='a' basic pointing='left'>
+              2,048
+            </Label>
+            </Button>
+            <MyModal visible={openModal} onClose={()=>{console.log("close"); setOpenModal(false);}} />
+            <Cambio/>
+          </div>
+        </Route>
+        <Route path = "/videos">
+          <div className="App">
+            <Menu inverted>
+              <Menu.Item
+                name='Ina'          
+                onClick={vidIna}
+              >
+                Ina
+              </Menu.Item>
+
+              <Menu.Item
+                name='Fubuki'
+                
+                onClick={vidFubuki}
+              >
+                Fubuki
+              </Menu.Item>
+
+              <Menu.Item
+                name='Marine'          
+                onClick={vidMarine}
+              >
+                Marine
+              </Menu.Item>
+            </Menu>
+            <Button >
+              <Link to="/">
+                Inicio
+              </Link>
+            </Button>
+            <Button>
+              <Link to="/videos">
+                Videos
+              </Link>
+            </Button>
+            <Button as='div' labelPosition='right' onClick={(e,d)=>setOpenModal(true)}>
+            <Button icon>
+              <Icon name='heart' />
+              Like
+            </Button>
+            <Label as='a' basic pointing='left'>
+              2,048
+            </Label>
+            </Button>
+            <MyModal visible={openModal} onClose={()=>{console.log("close"); setOpenModal(false);}} />
+            <hr />
+            <Video/>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
